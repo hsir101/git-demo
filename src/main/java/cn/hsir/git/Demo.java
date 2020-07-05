f7b79ec6eea3cee6a92643734f96a2772a08fd9e
hsir <hsir101@sian.com>
2020-07-05 21:42:41 +0800

    git的新代码

--- src/main/java/cn/hsir/git/Demo.java
+++ src/main/java/cn/hsir/git/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("git修改");
+        System.out.println("git的新代码");
     }
 }
